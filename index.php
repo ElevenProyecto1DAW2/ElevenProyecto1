a53f09d76a5e5acfd5baea4eb66b2fa24fdecbd6
Adrián <10000352.joan23@fje.edu>
2018-10-24 17:49:03 +0200

    Creacion de la estructura de archivos de la pagina web

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Casal Eleven</title>
+	<link rel="stylesheet" type="text/css" href="./css/styles.css">
+	<meta charset="utf-8">
+</head>
+<body>
+
+</body>
+</html>
