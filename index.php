2f9c2264e645b68dc429759b39e08a991817338e
Danny <10000364.joan23@fje.edu>
2018-10-24 20:36:46 +0200

    html y css V1

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 	<meta charset="utf-8">
 </head>
 <body>
+		<div class="menu"><h1 style="margin-top: 0px">Casal Eleven </h1></div>	
+		<div class="cuadro-central">
+	<div class="cuadrotexto">
+<h1  style="font-size: 50px; text-align: center; font-family:> Impact;">Quienes Somos</h1>
+		<p>Somos un casal donde disponemos de diferentes recursos para nuestros usuarios.</p>
+	</div>
+	<div class="cuadrotexto">
+<h1  style="font-size: 50px; text-align: center; font-family:> Impact;">Donde estamos</h1>
+	<iframe class="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.1176109768453!2d2.105329615064162!3d41.34979730628806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a498d64bd023fd%3A0x26089fc39cb352a3!2sJesu%C3%AFtes+Bellvitge.+Centre+d&#39;Estudis+Joan+XXIII!5e0!3m2!1ses!2ses!4v1540404591710" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</div>
+</div>
 
 </body>
 </html>
