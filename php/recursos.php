1a2169ace11de7d19a95b2e6f76bf1debd9e9793
Eric Peligro <megadangerss@gmail.com>
2018-10-26 17:30:26 +0200

    Actualizacion dia 3 agregamos correctamente los filtros y le damos estilos a la pagina principal

--- php/recursos.php
+++ php/recursos.php
@@ -14,9 +14,9 @@
 				</h1>
 				<p>Filtro para filtrar tomaa yaa!</p>
 				<div class="filtronuevo">
-					<form method="POST" name="FiltroRecursos" action="Filtrado.proc.php">
+					<form method="POST" name="FiltroRecursos" action="recursos.php">
 						<br>
-						<select name="Filtro" id="SeleccionSalaEquipo" onchange="OcultarMostrarFiltro()">
+						<select style="margin-left: 24%; width: 50%; height: 20%;" name="Filtro" id="SeleccionSalaEquipo">
 							<option value="-"> - </option>
 							<?php
 								$link = mysqli_connect('localhost', 'root', '', 'proyecto1eleven');
@@ -26,16 +26,36 @@
 									echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
 									exit;
 								}
-								$Sql="SELECT DISTINCT(Tipo_sala) from sala";
+								$Sql="SELECT DISTINCT(tipo_equipo_sala) from equipos_sala";
 								$Sql2=mysqli_query($link,$Sql);
 								ForEach ($Sql2 as $query) {
-									echo "<option value='$query[Tipo_sala]'>$query[Tipo_sala]</option>";
+									echo "<option value='$query[tipo_equipo_sala]'>$query[tipo_equipo_sala]</option>";
 								}
+								echo "<input type='hidden' name='usu' value='$_REQUEST[usu]'>";
 							?>
 						</select><br><br>
-						<input type="submit" name="Filtrado" value="Filtrar">
+						<input style="margin-left: 35%; margin-bottom: 5%;" type="submit" name="Filtrado" value="Filtrar">
 					</form>
+					<div class="filtrando">
+						<?php
+							
+								$link = mysqli_connect('localhost', 'root', '', 'proyecto1eleven');
+								if (!$link) {
+								    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+								    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+								    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+								    exit;
+								}
+								$Filtro="$_REQUEST[Filtro]";
+								$Sql="SELECT * FROM equipos_sala where tipo_equipo_sala='".$Filtro."'";
+								$Sql2=mysqli_query($link,$Sql);
+								ForEach ($Sql2 as $query) {
+									echo "<p style='color:white;'>$query[Id_equipo_sala] $query[tipo_equipo_sala]</p>";
+								}						
+						?>
+					</div>
 				</div>
+				
 			</div>
 		</div>
 	</body>
